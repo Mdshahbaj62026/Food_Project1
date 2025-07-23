467aeac86b39bb4f7ca8948a38f459b4f74c947b
Md Shahbaj <117162550+Mdshahbaj62026@users.noreply.github.com>
2025-07-23 14:19:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4501",
+      "/api": "https://food-project1.onrender.com",
     },
   },
   plugins: [react()],
